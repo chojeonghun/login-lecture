d1b25fe7c77d8b5645ba2217bb70ea5a5ebb94d9
Cho Jeong Hun <rednada999@naver.com>
2022-06-27 17:48:41 +0900

    루트의 src폴더 삭제

--- app/src/models/userStorage.js
+++ app/src/models/userStorage.js
@@ -32,6 +32,14 @@ class userStorage{
         return userInfo;
     }
 
+    static save(userInfo){
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.psword.push(userInfo.psword);
+        users.name.push(userInfo.name);
+        return {success: true};
+    }
+
 }
 
 module.exports = userStorage;
